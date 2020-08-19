aab23278de0c1990d048b6282de8580de415840a
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-19 21:23:37 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 def countdown(number)
   countdown_output = 10
   while countdown_output > 0 
-  puts "#{countdown_output} SECONDS!"
+  puts "#{countdown_output} SECOND (S)!"
   countdown_output -= 1
   end
   "HAPPY NEW YEARS!"
