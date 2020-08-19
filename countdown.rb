3dc6cf84d0592082c60942d9e718a02e54f58214
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-19 21:26:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
-
-def countdown
+num = 10
+def countdown(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep(num)
+    while num > 0
+        puts "#{num} SECOND(S)!"
+        sleep(1)
+        num -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
